6345b87d23346e1ef98d3f6d166afa7c16226dbe
Vinit Patidar <117593724+Vinit-patidar-03@users.noreply.github.com>
2023-10-01 21:20:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
     let success = false;
     try {
         const { name, email, message } = req.body;
